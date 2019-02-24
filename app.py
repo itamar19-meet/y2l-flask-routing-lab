dc3e527562c49d8cb6fbcaa690b24a4c5bc75176
aboud20-meet <aboud20@meet.mit.edu>
2019-02-24 19:17:27 +0200

    finished

--- app.py
+++ app.py
@@ -3,7 +3,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "You are home!"
+    return render_template("home.html")
+
+@app.route('/item')
+def item_page():
+    return render_template("item.html")
+
+    
+@app.route('/shop' , methods=['GET','POST'])
+def shop_page():
+    if(request.method == 'GET'):
+        return render_template("item.html" , request.form["item"])
+        
+    else:
+        return render_template("shop.html" , items("lamp","table","chair","computer")
+
+# @app.route('/item')
+# def 
+# @app.route('/item')
+# def item_page():
+#     return render_template("item.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
